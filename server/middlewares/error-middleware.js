5755d54d1211305593dd04fb9a719b3555a24e4c
suvmer <suvmer@mail.ru>
2023-07-20 01:13:00 +0300

    error handled debug message

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -1,7 +1,7 @@
 const ApiError = require("../exceptions/ApiError");
 
 module.exports = function(err, req, res, next) {
-    console.log(err)
+    console.log("Error handled: ", err.message)
     if(err instanceof ApiError)
         return res.status(err.status).json({status: false, message: err.message});
     return res.status(500).json({status: false, message: "Неизвестная ошибка"});
