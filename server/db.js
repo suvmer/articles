3187eb5ac9192e759f97b1107fac4604e8d29d80
suvmer <suvmer@mail.ru>
2023-07-20 01:00:38 +0300

    intellisense in vs code didn't work without that fix:c

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const Sequelize = require('sequelize')
+const {Sequelize} = require('sequelize')
 console.log(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, process.env.DBHOST)
 module.exports = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
     host: process.env.DBHOST,
