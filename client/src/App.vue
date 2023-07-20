549bac5177fdfa760f79e34a29473794ba302120
suvmer <suvmer@mail.ru>
2023-07-20 13:15:43 +0300

    vuetify installed + sidebar implemented

---
+++ client/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" class="flex-grow-0" hide-xl-only :elevation="1" app>
+      <v-app-bar-nav-icon v-if="isMobile()" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Articles Project</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-navigation-drawer disable-resize-watcher :permanent="!isMobile()" v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5">#ВСтатье</v-list-item-title>
+          <v-list-item-subtitle>Статьи и многое другое</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item title="Список статей" prepend-icon="mdi-view-dashboard" link/>
+        <v-list-item title="Комментарии" prepend-icon="mdi-account-box" link/>
+      </v-list>
+    </v-navigation-drawer>
+    
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    drawer: window.innerWidth >= 1200,
+    posts: [
+      {
+        id: 1,
+        title: "Nicer",
+        body: "Something happened"
+      },
+      {
+        title: "Dicer",
+        body: "Is amogus alive?"
+      }
+    ]
+  }),
+  methods: {
+    isMobile() {
+      return window.innerWidth < 1200;
+    }
+  }
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,2 +1,10 @@
 import { createApp } from 'vue'
-createApp(App).mount('#app')
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
