00bb00f4dbab0540b7bb0fb1533893ea22295399
suvmer <suvmer@mail.ru>
2023-07-22 00:17:19 +0300

    implemented postForm

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,9 +3,9 @@
     <v-app-bar color="primary" class="flex-grow-0" hide-xl-only :elevation="1" app>
       <v-app-bar-nav-icon v-if="mdAndDown" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-app-bar-title>Список статей</v-app-bar-title>
-      <v-spacer></v-spacer>
     </v-app-bar>
-    <v-navigation-drawer class="pa-2" :permanent="!mdAndDown" v-model="drawer" app>
+
+    <v-navigation-drawer absolute class="pa-2" :permanent="!mdAndDown" v-model="drawer">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h5">#ВСтатье</v-list-item-title>
@@ -16,60 +16,64 @@
       <v-list>
         <v-list-item title="Список статей" prepend-icon="mdi-view-dashboard" link/>
         <v-list-item title="Комментарии" prepend-icon="mdi-account-box" link/>
+        <v-btn block class="mx-1 mt-2" min-width="auto" href="https://github.com/suvmer/articles" target="_BLANK" prepend-icon="mdi-github">
+            GitHub
+        </v-btn>
       </v-list>
+      
     </v-navigation-drawer>
     <v-main>
-      <v-container class="pa-0" fluid>
-        <v-card
-          class="mx-auto my-2"
-          max-width="700"
-          v-for="post in posts"
-          :key="post.id"
-          :title="post.title"
-        >
-          <template v-slot:prepend>
-            <v-icon size="x-large">mdi-post-outline</v-icon>
-          </template>
+        <v-list max-width="700" class="pa-1 mx-auto">
+          <PostForm @createArticle="createArticle"/>
+          <v-card
+            class="mx-auto my-2"
+            v-for="post in posts"
+            :key="post.id"
+            :title="post.title"
+          >
+            <template v-slot:prepend>
+              <v-icon size="x-large">mdi-post-outline</v-icon>
+            </template>
 
-          <v-card-text class="text-h5 py-2">
-            {{ post.body }}
-          </v-card-text>
+            <v-card-text class="text-h5 py-2">
+              {{ post.body }}
+            </v-card-text>
 
-          <v-card-actions>
-            <v-list-item class="w-100">
-              <v-icon class="me-2" icon="mdi-comment-multiple-outline"/>
-              <span class="subheading me-2">{{post.comments.length}}</span>
-              <template v-slot:append>
-                <div class="justify-self-end">
-                  <v-list-item-subtitle>{{toDMY(post.createdAt)}}</v-list-item-subtitle>
-                </div>
-              </template>
-            </v-list-item>
-          </v-card-actions>
-        </v-card>
-        <v-card
-          class="mx-auto my-2"
-          max-width="700"
-          v-if="posts.length == 0 && !isLoading"
-          title="Нет статей"
-        />
-        <v-card
-          class="mx-auto my-2"
-          max-width="700"
-          v-if="posts.length == 0 && isLoading"
-          title="Загрузка..."
-        />
-        <v-overlay
-          :model-value="isLoading"
-          class="align-center justify-center"
-        >
-          <v-progress-circular
-            color="primary"
-            indeterminate
-            size="64"
-          ></v-progress-circular>
-        </v-overlay>
-      </v-container>
+            <v-card-actions>
+              <v-list-item class="w-100">
+                <v-icon class="me-2" icon="mdi-comment-multiple-outline"/>
+                <span class="subheading me-2">{{post.comments.length}}</span>
+                <template v-slot:append>
+                  <div class="justify-self-end">
+                    <v-list-item-subtitle>{{toDMY(post.createdAt)}}</v-list-item-subtitle>
+                  </div>
+                </template>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+          <v-card
+            class="mx-auto my-2"
+            max-width="700"
+            v-if="posts.length == 0 && !isLoading"
+            title="Нет статей"
+          />
+          <v-card
+            class="mx-auto my-2"
+            max-width="700"
+            v-if="posts.length == 0 && isLoading"
+            title="Загрузка..."
+          />
+          <v-overlay
+            :model-value="isLoading"
+            class="align-center justify-center"
+          >
+            <v-progress-circular
+              color="primary"
+              indeterminate
+              size="64"
+            ></v-progress-circular>
+          </v-overlay>
+        </v-list>
     </v-main>
   </v-app>
 </template>
@@ -78,15 +82,17 @@
 import axios from "axios";
 import { getCurrentInstance } from "vue";
 import { useDisplay } from "vuetify";
+import PostForm from "./components/PostForm";
 const vuetify = require('./plugins/vuetify')
 const {toDMY} = require('./utils');
 export default {
   name: 'App',
   components: {
+    PostForm
   },
 
   data: () => ({
-    drawer: window.innerWidth > 1280,
+    drawer: window.innerWidth >= 1280,
     isLoading: false,
     posts: []
   }),
@@ -99,24 +105,37 @@ export default {
     async fetchPosts() {
       try {
         this.isLoading = true;
-        const response = await axios.get('http://localhost:8000/articles')
+        const response = await axios.get('http://192.168.1.134:8000/articles')
         this.posts = response.data.posts.map(el => ({...el, comments: []}));
       } catch(e) {
         console.log(e);
       } finally {
         this.isLoading = false;
       }
-      this.fetchComments()
+      this.fetchAllComments()
     },
-    async fetchComments() {
+    async fetchAllComments() {
       try {
-        this.posts.forEach(async post => {
-          const response = await axios.get(`http://localhost:8000/article/${post.id}/comments`);
-          if(response.status == 200)
-            this.posts = this.posts.map(newpost => newpost.id === post.id ? ({...newpost, comments: [...response.data.comments]}) : newpost)
-        });
+        this.posts.forEach(async post => this.fetchComments(post));
       } catch(e) {
         console.log(e);
+      }
+    },
+    async fetchComments(post) {
+      const response = await axios.get(`http://192.168.1.134:8000/article/${post.id}/comments`);
+      if(response.status == 200)
+        this.posts = this.posts.map(newpost => newpost.id === post.id ? ({...newpost, comments: [...response.data.comments]}) : newpost)
+    },
+    async createArticle(post) {
+      try {
+        this.isLoading = true;
+        const response = await axios.post('http://192.168.1.134:8000/article', {...post})
+        this.posts = [...this.posts, ({...response.data.post, comments: []})];
+        this.fetchComments(response.data.post);
+      } catch(e) {
+        console.log(e);
+      } finally {
+        this.isLoading = false;
       }
     }
   },
---
+++ client/src/components/PostForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-card title="Создание статьи" class="pa-2">
+        <v-form ref="form" class="pa-2">
+            <v-text-field
+                v-model="post.title"
+                
+                label="Название статьи"
+                required
+            />
+            <v-textarea
+                v-model="post.body"
+                label="Содержание статьи"
+            />
+            <v-btn
+                color="success"
+                class="mt-1"
+                block
+                @click="addArticle"
+            >
+                Добавить статью
+            </v-btn>
+        </v-form>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        post: {
+            title: "",
+            body: ""
+        }
+    }),
+    methods: {
+        addArticle() {
+            this.$emit('createArticle', this.post);
+            this.post = {
+                title: "",
+                body: ""
+            };
+        }
+    }
+}
+</script>
