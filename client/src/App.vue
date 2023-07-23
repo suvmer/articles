0ef7f358353cfd99b3a2919a3476b9b35dc1a381
suvmer <suvmer@mail.ru>
2023-07-24 00:25:29 +0300

    added router links

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,42 +2,25 @@
   <v-app>
     <v-app-bar color="primary" class="flex-grow-0" hide-xl-only :elevation="1" app>
       <v-app-bar-nav-icon v-if="mdAndDown" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-bar-title>Список статей</v-app-bar-title>
+      <v-app-bar-title>{{ this.$route.name }}</v-app-bar-title>
     </v-app-bar>
 
     <v-navigation-drawer absolute class="pa-2" :permanent="!mdAndDown" v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h5">#ВСтатье</v-list-item-title>
-          <v-list-item-subtitle>Статьи и многое другое</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list>
-        <v-list-item title="Список статей" prepend-icon="mdi-view-dashboard" link/>
-        <v-list-item title="Комментарии" prepend-icon="mdi-account-box" link/>
+    <v-list-item>
+        <v-list-item-title class="text-h5"><a href="#">#ВСтатье</a></v-list-item-title>
+        <v-list-item-subtitle>Статьи и многое другое</v-list-item-subtitle>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list>
+        <router-link class="text-decoration-none on-surface" v-slot="{isActive}" to="/"><v-list-item :active="isActive" title="Список статей" prepend-icon="mdi-view-dashboard"/></router-link>
+        <router-link class="text-decoration-none on-surface" v-slot="{isActive}" to="/comments"><v-list-item :active="isActive" title="Комментарии" prepend-icon="mdi-comment-multiple-outline" link/></router-link>
         <v-btn block class="mx-1 mt-2" min-width="auto" href="https://github.com/suvmer/articles" target="_BLANK" prepend-icon="mdi-github">
             GitHub
         </v-btn>
-      </v-list>
-      
+    </v-list>
+    
     </v-navigation-drawer>
-    <v-main>
-        <v-list max-width="700" class="pa-1 mx-auto">
-          <PostForm @createArticle="createArticle"/>
-          <PostList @editComment="editComment" v-bind:posts="this.posts"/>
-          <v-overlay
-            :model-value="isLoading"
-            class="align-center justify-center"
-          >
-            <v-progress-circular
-              color="primary"
-              indeterminate
-              size="64"
-            ></v-progress-circular>
-          </v-overlay>
-        </v-list>
-    </v-main>
+    <RouterView/>
   </v-app>
 </template>
 
