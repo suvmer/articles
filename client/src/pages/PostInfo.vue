60a2b8bd34353c8caca559b4f741eb653978a50e
suvmer <suvmer@mail.ru>
2023-07-25 00:32:55 +0300

    PostInfo page added with related vuex mutations and actions

---
+++ client/src/pages/PostInfo.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-main>
+        <Post v-if="getPost != null" :expanded="true" v-bind:post="getPost"/>
+        <v-list max-width="700" class="pa-1 mx-auto">
+            <v-overlay
+                :model-value="this.$store.state.article.isLoading"
+                class="align-center justify-center"
+            >
+                <v-progress-circular
+                color="primary"
+                indeterminate
+                size="64"
+                ></v-progress-circular>
+            </v-overlay>
+        </v-list>
+    </v-main>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  import Post from "../components/Post";
+  
+  export default {
+    name: 'App',
+    components: {
+        Post
+    },
+    props: {
+        id: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        getPost() {
+            if(this.$store.state.article.postToShow?.id == this.id) {
+                console.log(this.$store.state.article.kek);
+                return this.$store.state.article.postToShow;
+            }
+            return null;
+        }
+    },
+    methods: {
+      
+    },
+    mounted() {
+        this.$store.dispatch('fetchPost', this.id);
+    }
+  }
+  </script>
--- client/src/store/articleModule.js
+++ client/src/store/articleModule.js
@@ -2,15 +2,31 @@ import axios from "axios";
 
 export const articleModule = {
     state: () => ({
-        kek: 5
+        isLoading: false,
+        postToShow: null
     }),
     getters: {
-
+        
     },
     mutations: {
-
+        setPostToShow(state, post) {
+            state.postToShow = post;
+        },
+        setLoading(state, value) {
+            state.isLoading = value;
+        }
     },
     actions: {
-
+        async fetchPost({state, commit}, id) {
+            try {
+                commit("setLoading", true);
+                const response = await axios.get(`http://192.168.1.134:8000/article/${id}`)
+                commit("setPostToShow", response.data.post);
+            } catch(e) {
+                console.log(e);
+            } finally {
+                commit("setLoading", false);
+            }
+        },
     }
 }
