054bbf3e4a260c77932389c69ec96a95bf6b23e8
suvmer <suvmer@mail.ru>
2023-07-24 18:57:34 +0300

    migration works

--- server/migrations/20230724154222-Create-Posts-And-Comments.js
+++ server/migrations/20230724154222-Create-Posts-And-Comments.js
@@ -1,41 +1,46 @@
 'use strict';
 
+const Comments = require('../models/Comments');
+const Posts = require('../models/Posts');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Comments', {
+    await queryInterface.createTable(Posts.tableName, Posts.getAttributes(), Posts.options);
+    await queryInterface.createTable(Comments.tableName, Comments.getAttributes(), Comments.options);
+    /*await queryInterface.createTable('Comments', {
       id: {
-          type: DataTypes.INTEGER,
+          type: Sequelize.DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true,
           allowNull: false
       },
       body: {
-          type: DataTypes.STRING,
+          type: Sequelize.DataTypes.STRING,
           allowNull: true
       },
       post_id: {
-          type: DataTypes.INTEGER,
+          type: Sequelize.DataTypes.INTEGER,
           allowNull: false
       }
   });
 
   await queryInterface.createTable('Posts', {
     id: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false
     },
     title: {
-        type: DataTypes.STRING,
+        type: Sequelize.DataTypes.STRING,
         allowNull: false
     },
     body: {
-        type: DataTypes.STRING,
+        type: Sequelize.DataTypes.STRING,
         allowNull: true
     }
-});
+});*/
 
 
      
