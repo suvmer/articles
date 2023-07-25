7ad263e7554ec45e8efff3fbb4d96b61d49f3a93
suvmer <suvmer@mail.ru>
2023-07-25 09:32:57 +0300

    +comment reducer + ui reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex";
 import {articleModule} from "./articleModule"
+import { commentsModule } from "./commentsModule";
+import { UIModule } from "./UIModule";
 
 export default createStore({
     modules: {
-        article: articleModule
+        article: articleModule,
+        comment: commentsModule,
+        ui: UIModule
     }
 })
