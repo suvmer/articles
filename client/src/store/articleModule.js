2f65d44d95e2663c83ba2f7a553c9e646d2b6da8
suvmer <suvmer@mail.ru>
2023-07-25 09:35:00 +0300

    implemented CRUD for articles

--- client/src/store/articleModule.js
+++ client/src/store/articleModule.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const articleModule = {
     state: () => ({
-        isLoading: false,
-        postToShow: null
+        postToShow: null,
+        posts: []
     }),
     getters: {
         
@@ -12,8 +12,11 @@ export const articleModule = {
         setPostToShow(state, post) {
             state.postToShow = post;
         },
-        setLoading(state, value) {
-            state.isLoading = value;
+        setPosts(state, posts) {
+            state.posts = posts;
+        },
+        removePost(state, id) {
+            state.posts = state.posts.filter(post => post.id != id);
         }
     },
     actions: {
@@ -28,5 +31,48 @@ export const articleModule = {
                 commit("setLoading", false);
             }
         },
+        async fetchPosts({state, commit}) {
+            try {
+                commit('setLoading', true);
+                const response = await axios.get('http://192.168.1.134:8000/articles')
+                commit('setPosts', response.data.posts);
+            } catch(e) {
+                console.log(e);
+            } finally {
+                commit('setLoading', false);
+            }
+        },
+        async editPost({state, commit}, post) {
+          try {
+              await axios.patch(`http://192.168.1.134:8000/article/${post.id}/`, {...post})
+          } catch(e) {
+              console.log(e);
+              if(e.response?.data?.message)
+                  alert(e.response.data.message)
+          }
+        },
+        async deletePost({state, commit}, post) {
+          try {
+              await axios.delete(`http://192.168.1.134:8000/article/${post.id}/`)
+              commit('removePost', post.id)
+          } catch(e) {
+              console.log(e);
+              if(e.response?.data?.message)
+                  alert(e.response.data.message)
+          }
+        },
+        async createPost({state, commit}, post) {
+            try {
+                commit('setLoading', true);
+                const response = await axios.post('http://192.168.1.134:8000/article', {...post})
+                commit('setPosts',  [...state.posts, response.data.post]);
+            } catch(e) {
+                console.log(e);
+                if(e.response.data?.message)
+                    alert(e.response.data?.message)
+            } finally {
+                commit('setLoading', false);
+            }
+        }
     }
 }
