365b18a9ae4fdf1f128e3e4062797681822b55a6
suvmer <suvmer@mail.ru>
2023-07-25 09:28:48 +0300

    editing and deleting post via vuex

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -3,20 +3,33 @@
         <v-card
             class="mx-auto my-2"
             :key="post.id"
-            :to='"/post/"+post.id'
+            :to='(isEditing||expanded) ? "" : "/post/"+post.id'
         >
             <template v-slot:title>
-                <v-card-title :class="expanded ? 'text-wrap' : ''">{{post.title}}</v-card-title>
+                <v-card-title :class="expanded ? 'text-wrap' : ''">{{ !isEditing ? newPost.title : "" }}</v-card-title>
+                <v-text-field
+                    v-if="isEditing"
+                    v-model="newPost.title"
+                    label="Название статьи"
+                    required
+                />
             </template>
             <template v-slot:prepend>
                 <v-icon size="x-large">mdi-post-outline</v-icon>
             </template>
             <template v-slot:append>
-                <v-icon size="x-large">mdi-pencil</v-icon>
+                <v-icon v-if="!isEditing" @click="$event.preventDefault(); isEditing = true">mdi-pencil</v-icon>
+                <v-icon v-else @click="isEditing = false; editPost()">mdi-check-bold</v-icon>
+                <DeleteIcon class="ms-2" @onDelete="this.$store.dispatch('deletePost', this.newPost); this.$router.back();"/>
             </template>
 
             <v-card-text style="line-height: 1.7rem;" class="text-h5 py-2 ">
-                {{ post.body }}
+                <v-list-item-title class="text-wrap">{{ !isEditing ? newPost.body : "" }}</v-list-item-title>
+                <v-textarea
+                    v-if="isEditing"
+                    v-model="newPost.body"
+                    label="Текст статьи"
+                />
             </v-card-text>
 
             <v-card-actions>
@@ -36,9 +49,12 @@
 </template>
 
 <script>
+import DeleteIcon from './DeleteIcon'
 const {toDMY} = require('../utils');
 export default {
-    components: {},
+    components: {
+        DeleteIcon
+    },
     beforeCreate() { //HACK to avoid circular dependency
        this.$options.components.CommentsList = require('./CommentsList').default
     },
@@ -56,8 +72,19 @@ export default {
             default: false
         }
     },
+    mounted() {
+        this.newPost = this.post;
+    },
+    data: () => ({
+        newPost: {},
+        isEditing: false
+    }),
     methods: {
         toDMY: (timestamp) => toDMY(timestamp),
+        editPost() {
+            this.newPost = {...this.newPost, updatedAt: Date.now().valueOf()}
+            this.$store.dispatch('editPost', this.newPost)
+        }
     }
 }
 </script>
