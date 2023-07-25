7fdc814e94526d335aca30c53375cc216acf8b0c
suvmer <suvmer@mail.ru>
2023-07-25 09:30:21 +0300

    added button that needs 3 clicks to work

---
+++ client/src/components/DeleteIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-icon :class="$attrs.class" v-if="stage == 0" @click="this.stage = 1">mdi-delete</v-icon>
+    <v-icon :class="$attrs.class" v-if="stage == 1" @click="this.stage = 2">mdi-delete-sweep</v-icon>
+    <v-icon :class="$attrs.class" v-if="stage == 2" @click="this.$emit('onDelete'); this.stage = 0">mdi-delete-alert</v-icon>
+</template>
+
+<script>
+export default {
+    data: () => ({
+       stage: 0
+    })
+}
+</script>
