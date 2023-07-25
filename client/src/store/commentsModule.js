bde12787b94826a76f2b67b6a855df85c9d6c2f8
suvmer <suvmer@mail.ru>
2023-07-25 09:32:39 +0300

    new reducer for comments

---
+++ client/src/store/commentsModule.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const commentsModule = {
+    state: () => ({
+        
+    }),
+    getters: {
+        
+    },
+    mutations: {
+        
+    },
+    actions: {
+        async editComment({state, commit}, comment) {
+          try {
+              await axios.patch(`http://192.168.1.134:8000/article/${comment.post_id}/comment/${comment.id}`, {...comment})
+          } catch(e) {
+              console.log(e);
+              if(e.response?.data?.message)
+                  alert(e.response.data.message)
+          }
+        }
+    }
+}
