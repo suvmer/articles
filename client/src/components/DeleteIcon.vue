20d771bed6bfe0ae49c34a043764b136859a90e7
suvmer <suvmer@mail.ru>
2023-07-25 09:36:58 +0300

    cancel event on click on delete button

--- client/src/components/DeleteIcon.vue
+++ client/src/components/DeleteIcon.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-icon :class="$attrs.class" v-if="stage == 0" @click="this.stage = 1">mdi-delete</v-icon>
-    <v-icon :class="$attrs.class" v-if="stage == 1" @click="this.stage = 2">mdi-delete-sweep</v-icon>
-    <v-icon :class="$attrs.class" v-if="stage == 2" @click="this.$emit('onDelete'); this.stage = 0">mdi-delete-alert</v-icon>
+    <v-icon :class="$attrs.class" v-if="stage == 0" @click="$event.preventDefault();this.stage = 1">mdi-delete</v-icon>
+    <v-icon :class="$attrs.class" v-if="stage == 1" @click="$event.preventDefault();this.stage = 2">mdi-delete-sweep</v-icon>
+    <v-icon :class="$attrs.class" v-if="stage == 2" @click="$event.preventDefault();this.$emit('onDelete'); this.stage = 0">mdi-delete-alert</v-icon>
 </template>
 
 <script>
