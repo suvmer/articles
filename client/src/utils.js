031d91195c352b1c575dfb325bf3e6e1339a1c8b
suvmer <suvmer@mail.ru>
2023-07-26 11:39:07 +0300

    new func to normalize data(hours=minutes=0)

--- client/src/utils.js
+++ client/src/utils.js
@@ -9,3 +9,7 @@ export const toDMY = (timestamp) => {
     const date = new Date(timestamp);
     return `${toWidth(date.getDate(), 2)}.${toWidth(date.getMonth() + 1, 2)}.${date.getFullYear()} ${toWidth(date.getHours(), 2)}:${toWidth(date.getMinutes(), 2)}`;
 }
+export const normalizeDate = (data) => {
+    const newDate = new Date(data);
+    return new Date(data.valueOf() - (newDate.getHours()*60 + newDate.getMinutes())*60*1000);
+}
