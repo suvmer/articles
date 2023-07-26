3167f40737e20a6eb1205b388ab5bc0ad0aee148
suvmer <suvmer@mail.ru>
2023-07-26 21:00:01 +0300

    comments cleaned

--- server/migrations/20230724154222-Create-Posts-And-Comments.js
+++ server/migrations/20230724154222-Create-Posts-And-Comments.js
@@ -8,42 +8,6 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable(Posts.tableName, Posts.getAttributes(), Posts.options);
     await queryInterface.createTable(Comments.tableName, Comments.getAttributes(), Comments.options);
-    /*await queryInterface.createTable('Comments', {
-      id: {
-          type: Sequelize.DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false
-      },
-      body: {
-          type: Sequelize.DataTypes.STRING,
-          allowNull: true
-      },
-      post_id: {
-          type: Sequelize.DataTypes.INTEGER,
-          allowNull: false
-      }
-  });
-
-  await queryInterface.createTable('Posts', {
-    id: {
-        type: Sequelize.DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    title: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false
-    },
-    body: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: true
-    }
-});*/
-
-
-     
   },
 
   async down (queryInterface, Sequelize) {
