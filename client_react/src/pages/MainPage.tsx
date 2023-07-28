6c5d9e030ef3e252f9caa1488afc76c458cf84df
suvmer <suvmer@mail.ru>
2023-07-29 00:33:25 +0300

    Проверка получения постов

--- client_react/src/pages/MainPage.tsx
+++ client_react/src/pages/MainPage.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react'
+import { useTypedSelector } from "../hooks/useTypedSelector"
+import { fetchPosts } from "../store/action-creators/post"
+import { useAppDispatch } from '../hooks/useAppDispatch';
+
 export const MainPage = () => {
-    return <div>Main page</div>
+    const dispatch = useAppDispatch();
+    useEffect(() => { dispatch(fetchPosts()) }, []);
+    const {loading, posts} = useTypedSelector(state => state.posts);
+    const postList = loading ? "Загрузка" : posts.map(post => <div style={{padding: "10px", margin: '10px', border: "1px solid black"}}><p>{post.title}</p><p>{post.body}</p><br/></div>);
+    return <div>Main page<div>{postList}</div></div>
 }
