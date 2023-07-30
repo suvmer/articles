fbc9113843afb0b1fa2f3e4dac117534b95a74b1
suvmer <suvmer@mail.ru>
2023-07-30 07:43:29 +0300

    Убраны лишние импорты

--- client_react/src/pages/MainPage.tsx
+++ client_react/src/pages/MainPage.tsx
@@ -1,23 +1,19 @@
-import { ChangeEvent, FC, useCallback, useEffect, useMemo, useState } from 'react'
+import { FC, useEffect, useMemo } from 'react'
 import { useTypedSelector } from "../hooks/useTypedSelector"
 import { fetchPosts } from "../store/action-creators/post"
 import { useAppDispatch } from '../hooks/useAppDispatch';
-import { useNavigate } from 'react-router';
 import '../style.css';
-import { PostCard } from '../components/PostCard';
 import { PostList } from '../components/PostList';
 import { PostForm } from '../components/PostForm';
-import { PostFormValue } from '../types/post';
 
-export const MainPage : FC = () => {
+export const MainPage : FC = (props) => {
     const dispatch = useAppDispatch();
-    useEffect(() => { console.log("wtf"); dispatch(fetchPosts()) }, []);
+    useEffect(() => { dispatch(fetchPosts()) }, []);
     const {loading, posts} = useTypedSelector(state => state.posts);
     const MemoPostForm = useMemo(() => <PostForm/>, []);
     const postList = (loading && !posts) ?
         "Загрузка" :
         <PostList posts={posts}/>
-        //posts.map(post => <PostCard post={post}></PostCard>);
     return <>
         {MemoPostForm}
         {postList}
