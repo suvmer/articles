463c0f95b885983f90b7ba6117c204bdf85eb8e4
suvmer <suvmer@mail.ru>
2023-08-02 17:59:37 +0300

    Исправлен баг со сменой конечной даты календаря на следующую

--- client_react/src/utils.ts
+++ client_react/src/utils.ts
@@ -4,17 +4,16 @@ export const normalizeDate = (data:Date) : Date => {
     const newDate = new Date(data);
     return new Date(data.valueOf() - (newDate.getHours()*60 + newDate.getMinutes())*60*1000);
 }
-//example: 16.04.23 15:30 => 16.04.23 00:00
+//example: 16.04.23 15:30 => 16.04.23 00:00 МИНУС 1001 микросекунд, чтобы в календаре дата не менялась на следующую(выглядело бы странно) 
 export const normalizeTimeStamp = (timestamp:number) : number => {
     const date = new Date(timestamp);
-    return (timestamp - (date.getHours()*60 + date.getMinutes())*60*1000 - date.getSeconds()*1000);
+    return (timestamp - (date.getHours()*60 + date.getMinutes())*60*1000 - date.getSeconds()*1000 - 1001);
 }
 
 export const toWidth = (num:number, length:number, toRepeat:string = "0") => {
     const str = num.toString()
     if(str.length < length)
         return toRepeat.repeat(length - str.length) + str;
-    
     return str;
 }
 export const toDMY = (timestamp:number) => {
